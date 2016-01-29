2227be08ebcecf2a842d83269f398353dfee68cd
Chris Coffey <cmcof273@gmail.com>
2016-01-29 16:27:50 -0500

    add git funcs and such

---
+++ git/gitFuncs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+# https://github.com/mfitzp/martinfitzpatrick.name/blob/master/content/computing/add-git-branch-name-to-terminal-prompt-mac.md
+parse_git_branch() {
+
+      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+
+    }
+
+export PS1="\u@\h \W\[\033[32m\]\$(parse_git_branch)\[\033[00m\] $ "
+
+function gwl(){
+  Command="git branch --merged master | grep -v master " 
+  
+  if [ "$1" == "d" ]; then
+    Command+="| xargs -I % sh -c 'git branch -D %; git push origin --delete %;'"
+  fi;
+ 
+  eval "$Command && git fetch --prune origin"
+}
+
+if [ -f `brew --prefix`/etc/bash_completion ]; then
+        . `brew --prefix`/etc/bash_completion
+fi
+
+alias gc="git checkout"
+alias gcm="git checkout master"
+alias gcd="git checkout develop"
+alias gp="git pull"
