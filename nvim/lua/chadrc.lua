7b7328358bd9085ee7cd93d67d055becea59e75d
Chris Coffey <chris@collegevine.com>
2025-03-01 21:15:28 -0500

    Add purescript filetype

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "ayu_light",
+	theme = "github_dark",
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
---
+++ nvim/lua/configs/filetypes.lua
@@ -0,0 +1,7 @@
+vim.filetype.add(
+  {
+    extension = {
+      purs = "purescript"
+    }
+  }
+)
